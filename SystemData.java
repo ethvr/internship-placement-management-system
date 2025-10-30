7fdef8ac3b5af3d601a3f745ad717d01811925a4
ethvr <99084824+ethvr@users.noreply.github.com>
2025-10-30 15:08:12 +0800

    SystemData

---
+++ SystemData.java
@@ -0,0 +1,6 @@
+public class SystemData {
+    public List<User> users = new ArrayList<>();
+    public List<Internship> internships = new ArrayList<>();
+    public List<Application> applications = new ArrayList<>();
+    public List<WithdrawalRequest> withdrawalRequests = new ArrayList<>();
+}
