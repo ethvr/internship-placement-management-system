eada6a152440d37846779cc6d46815d88370ee37
jiashun <jiashun@jiashuns-Laptop.local>
2025-11-09 14:42:22 +0800

    Add IdGenerator.java

---
+++ IdGenerator.java
@@ -0,0 +1,23 @@
+public class IdGenerator {
+
+     private static int internshipCounter = 0;
+     private static int applicationCounter = 0;
+     private static int withdrawalCounter = 0; 
+
+     
+     public static String nextInternshipId() {
+          internshipCounter++;
+          return String.format("I%04d", internshipCounter);
+     }
+     
+     public static String nextAppId() {
+          applicationCounter++;
+          return String.format("A%04d", applicationCounter);
+     }
+     
+     public static String nextWithdrawalId() {
+          withdrawalCounter++;
+          return String.format("W%04d", withdrawalCounter);
+     }
+     
+}
